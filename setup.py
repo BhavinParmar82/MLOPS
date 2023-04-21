efdd0ae8380a2f1da001369ea5b7ff33fb9d4791
BhavinParmar82 <bhavin9922@gmail.com>
2023-04-21 12:52:30 +0530

    first attempt1

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
-from setuptools import find_packages, setup
 from typing import List
 
+from setuptools import find_packages, setup
+
 hypen_const = "-e ."
-def get_requires(file_path:str)->List[str]:
-    '''
+
+
+def get_requires(file_path: str) -> List[str]:
+    """
     This function will return all package requirements as a list
-    '''
+    """
     requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
@@ -13,14 +16,14 @@ def get_requires(file_path:str)->List[str]:
         if hypen_const in requirements:
             requirements.remove(hypen_const)
     return requirements
-    
+
 
 setup(
-    name='src',
+    name="src",
     packages=find_packages(),
-    version='0.1.0',
-    description='MLOPs testing purpose',
-    author='Bhavin Parmar',
-    license='',
-    install_requires = get_requires("requirements.txt")
+    version="0.1.0",
+    description="MLOPs testing purpose",
+    author="Bhavin Parmar",
+    license="",
+    install_requires=get_requires("requirements.txt"),
 )
