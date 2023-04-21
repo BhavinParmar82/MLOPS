a5a174a7f90f7f69ce1dad171bdc68d0ad1b1598
BhavinParmar82 <bhavin9922@gmail.com>
2023-04-21 10:57:48 +0530

    packages install using requirements.txt and setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,19 @@
 from setuptools import find_packages, setup
+from typing import List
+
+hypen_const = "-e ."
+def get_requires(file_path:str)->List[str]:
+    '''
+    This function will return all package requirements as a list
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if hypen_const in requirements:
+            requirements.remove(hypen_const)
+    return requirements
+    
 
 setup(
     name='src',
@@ -7,4 +22,5 @@ setup(
     description='MLOPs testing purpose',
     author='Bhavin Parmar',
     license='',
+    install_requires = get_requires("requirements.txt")
 )
